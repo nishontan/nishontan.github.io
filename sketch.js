65d3527f5f95e92f920f52fea486e30993c8db19
Nishon Tandukar <nishon.tan@gmail.com>
2018-11-08 11:47:44 +0545

    First letter different clor in stream

--- sketch.js
+++ sketch.js
@@ -1,6 +1,9 @@
 var symbolSize = 24;
 var streams = [];
 
+window.onload = showViewport;
+window.onresize = showViewport;
+
 function setup() {
     createCanvas(
         window.innerWidth,
@@ -9,7 +12,7 @@ function setup() {
     background(0);
 
     var x = 0;
-    var y = 0;
+    var y = random(-1000,0);
 
     for (var i = 0; i < width / symbolSize; i++) {
         var stream = new Stream();
@@ -21,18 +24,27 @@ function setup() {
 }
 
 function draw() {
-    background(0)
+    background(0,200)
     
     streams.forEach(function(stream){
         stream.render();
     });
 }
 
-function Symbol(x, y, speed) {
+
+function showViewport() {
+    var output=document.getElementById("output");
+    var width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    var height= Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
+    output.innerHTML = "Viewport size is " + width + "x" + height;
+  }
+
+function Symbol(x, y, speed,position) {
     this.x = x;
     this.y = y;
     this.value;
     this.speed = speed;
+    this.position = position;
     this.switchInterval = round(random(1, 150));
 
     //todo: get nepali/newari char instead
@@ -61,13 +73,10 @@ function Symbol(x, y, speed) {
 function Stream() {
     this.symbols = [];
     this.totalSymbols = round(random(1, 30));
-    this.speed = random(5, 10);
-
-    console.log(this.totalSymbols)
-
+    this.speed = random(5, 30);
     this.generateSymbols = function (x,y) {
         for (var i = 0; i <= this.totalSymbols; i++) {
-            symbol = new Symbol(x, y, this.speed);
+            symbol = new Symbol(x, y, this.speed,i);
             symbol.setToRandomSymbol();
             this.symbols.push(symbol);
             y -= symbolSize;
@@ -76,7 +85,11 @@ function Stream() {
 
     this.render = function () {
         this.symbols.forEach(function (symbol) {
-            fill(0, 255, 70)
+            if(symbol.position === 0){
+                fill(180,255,180);
+            }else{
+                fill(0, 255, 10)
+            }
             text(symbol.value, symbol.x, symbol.y)
             symbol.rain();
             symbol.setToRandomSymbol();
