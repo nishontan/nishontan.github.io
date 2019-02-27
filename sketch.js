b76ff845115ff844ebaff94e67f157d8a9aab65c
Nishon Tandukar <nishon.tan@gmail.com>
2019-02-27 08:51:44 +0545

    routine commit

--- sketch.js
+++ sketch.js
@@ -1,8 +1,7 @@
 var symbolSize = 24;
 var streams = [];
-
-window.onload = showViewport;
-window.onresize = showViewport;
+var fadeInterval = 4;
+var message = "न्हू दँ 1139 या यकु यकु भिन्तुना"
 
 function setup() {
     createCanvas(
@@ -12,34 +11,35 @@ function setup() {
     background(0);
 
     var x = 0;
-    var y = random(-1000,0);
+    var y = random(-1000, 0);
 
     for (var i = 0; i < width / symbolSize; i++) {
         var stream = new Stream();
-        stream.generateSymbols(x,y);
+        stream.generateSymbols(x, y);
         streams.push(stream);
         x += symbolSize
     }
     textSize(symbolSize);
+
+
+
 }
 
 function draw() {
-    background(0,200)
-    
-    streams.forEach(function(stream){
+    background(0, 200)
+
+    // var vintuna = new Symbol(0,0,2,1);
+    // vintuna.setToRandomSymbol();
+    // vintuna.render();
+
+    streams.forEach(function (stream) {
         stream.render();
     });
+
+
 }
 
-
-function showViewport() {
-    var output=document.getElementById("output");
-    var width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-    var height= Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
-    output.innerHTML = "Viewport size is " + width + "x" + height;
-  }
-
-function Symbol(x, y, speed,position) {
+function Symbol(x, y, speed, position) {
     this.x = x;
     this.y = y;
     this.value;
@@ -53,9 +53,13 @@ function Symbol(x, y, speed,position) {
     this.setToRandomSymbol = function () {
         if (frameCount % this.switchInterval == 0) {
             this.value = String.fromCharCode(
-                0x30A0 + round(random(0, 96))
+                0x0915 + round(random(0, 24)) 
             );
         }
+    }
+
+    this.setText = function(value){
+        this.value = value;
     }
 
     this.render = function () {
@@ -73,22 +77,32 @@ function Symbol(x, y, speed,position) {
 function Stream() {
     this.symbols = [];
     this.totalSymbols = round(random(1, 30));
-    this.speed = random(5, 30);
-    this.generateSymbols = function (x,y) {
+    this.speed = random(5, 20);
+
+    this.generateSymbols = function (x, y) {
+        var opacity = 255;
+
         for (var i = 0; i <= this.totalSymbols; i++) {
-            symbol = new Symbol(x, y, this.speed,i);
+            symbol = new Symbol(
+                x,
+                y,
+                this.speed,
+                i,
+                opacity
+            );
             symbol.setToRandomSymbol();
             this.symbols.push(symbol);
+            opacity -= (255 / this.totalSymbols) / fadeInterval;
             y -= symbolSize;
         }
     }
 
     this.render = function () {
         this.symbols.forEach(function (symbol) {
-            if(symbol.position === 0){
-                fill(180,255,180);
-            }else{
-                fill(0, 255, 10)
+            if (symbol.position === 0 ) {
+                fill(180, 255, 180);
+            } else {
+                fill(0, 255, 10, 100)
             }
             text(symbol.value, symbol.x, symbol.y)
             symbol.rain();
